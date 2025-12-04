0c7ac7728977c47041210be295fd2d3ee45dc4b6
Ender <ndxendernight@gmail.com>
2025-12-05 00:26:20 +0100

    fix: day1

--- day1/part2.py
+++ day1/part2.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 with open("input") as input:
-    instructions = [
-        (line[0], int(line[1:]))
-        for line in [line.strip() for line in input.readlines()]
-    ]
+    instructions = [(line[0], int(line[1:])) for line in input.readlines()]
 
     pos = 50
     zero_count = 0
 
     for direction, distance in instructions:
         if direction == "L":
-            new_pos = pos - distance
+            new_pos = (100 if pos == 0 else pos) - distance
 
             pos = new_pos % 100
-            zero_count += abs((new_pos + 1) // 100)  # this only works on my input
+            zero_count += abs(new_pos // 100)
+
+            if pos == 0:
+                zero_count += 1
         else:
             new_pos = pos + distance
 
